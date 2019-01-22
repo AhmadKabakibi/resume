4cff2e9a42f4e1dee8e5d38af0d5b07a92011cee
Ahmad Kabakibi <ahmadkbakibi@gmail.com>
2019-01-22 16:06:50 +0100

    Update gatsby config and plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,18 @@
+const config = require('./src/config')
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: config.title,
+    siteUrl: config.url,
+    description: config.description,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -11,20 +20,48 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'akabakibi',
+        short_name: 'akabakibi',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#020c1b',
+        theme_color: '#0a192f',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/logo.png',
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow noopener noreferrer',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1100,
+              quality: 90,
+              linkImagesToOriginal: true,
+            },
+          },
+        ],
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/src/content/`,
+      },
+    },
   ],
 }
